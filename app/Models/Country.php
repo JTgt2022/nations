4e49340c5ff2eed522c71c25e4acc88d144ec478
Your Name <you@example.com>
2022-08-04 11:56:48 -0500

    modelos añadidos

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -7,13 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-
-    //table relacionada,
-   protected $table = "countries";
-   //primery key of the table,
-   protected $primaryKey= "country_id";
-   //ausencia de campo de fecha,
-   public $timestamps= false; 
-
     use HasFactory;
 }
--- app/Models/Languaje.php
+++ app/Models/Languaje.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Continent extends Model
+class Languaje extends Model
 {
-    protected $table = "continents";
     use HasFactory;
 }
